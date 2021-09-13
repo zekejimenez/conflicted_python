4a7de3ea865416bea88ce2ce720fbcbd5e6d83e8
Jeremy Roberts <jaroberts@ksu.edu>
2021-09-13 10:33:31 -0500

    Added working version to master

---
+++ demo.py
@@ -0,0 +1,13 @@
+""" Demo file """
+
+import glob
+
+def print_files(directory):
+    files = glob.glob(directory) 
+    print(files)
+    for i, f in enumerate(files):
+        print('file {} is {}'.format(i, f))
+
+if __name__ == '__main__':
+    directory = "/Users/robertsj/*"
+    print_files(directory)
